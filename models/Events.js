8c1ad06bc4303e0b4877b9189784026860096822
Nícholas Cassano <ninocb@gmail.com>
2022-10-05 16:10:23 -0300

    update evento.js

--- models/Events.js
+++ models/Events.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const EventSchema = new Schema ({
-    title:{
-        type: String,
-        require: true
-    },
-    content:{
+    titulo:{
         type: String,
         require: true
     },
